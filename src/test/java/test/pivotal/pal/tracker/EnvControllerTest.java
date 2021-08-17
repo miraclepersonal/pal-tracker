e24ad66a427f2903f5500b57ba71aa0f3e53d276
Ubuntu <ubuntu@ip-172-31-95-232.ec2.internal>
2021-08-17 19:46:15 +0000

    pal-tracker changes

--- src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
@@ -1,20 +1,20 @@
 package test.pivotal.pal.tracker;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.Map;
 import io.pivotal.pal.tracker.EnvController;
+import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EnvControllerTest {
     @Test
     public void getEnv() {
+
         EnvController controller = new EnvController(
-            "8675",
-            "12G",
-            "34",
-            "123.sesame.street"
+                "8675",
+                "12G",
+                "34",
+                "123.sesame.street"
         );
 
         Map<String, String> env = controller.getEnv();
