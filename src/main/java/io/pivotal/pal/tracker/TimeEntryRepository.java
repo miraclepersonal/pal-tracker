a384ff666654cbb49739da73446f6c31b1ffe7a2
Ubuntu <ubuntu@ip-172-31-95-232.ec2.internal>
2021-08-18 18:25:10 +0000

    Spring Rest MVC

--- src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
@@ -1,7 +1,13 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Repository
 public interface TimeEntryRepository {
     TimeEntry create(TimeEntry timeEntry);
     TimeEntry find(long id);
