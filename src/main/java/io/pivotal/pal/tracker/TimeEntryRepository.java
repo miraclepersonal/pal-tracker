2581311cd2b3feeaf0e6e53b761235998d611e13
Ubuntu <ubuntu@ip-172-31-95-232.ec2.internal>
2021-08-19 01:49:26 +0000

    MYSQL Migration through Flyway

--- src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
@@ -1,10 +1,5 @@
 package io.pivotal.pal.tracker;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 public interface TimeEntryRepository {
